589c9adf24996bfe476c1302170d3a05bdf4f9a2
Victor Codonho <victorenrico50@gmail.com>
2021-06-05 23:57:46 -0300

    teste realizado

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -1,12 +1,27 @@
+import { IncomingHttpHeaders } from 'http';
 import { Request, Response } from "express";
 
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
+
+declare module 'http' {
+    interface IncomingHttpHeaders {
+        "user_id"?: string
+    }
+}
 
 class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+
+    try {
+      const users = this.listAllUsersUseCase.execute({ user_id });
+
+      return response.json(users);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   }
 }
 
